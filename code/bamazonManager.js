93dad097b8782cd29794db48d78d289af0a4ea63
JLasc <lascaze.j@gmail.com>
2018-09-05 16:23:54 -0400

    removed user/pass prompt

--- code/bamazonManager.js
+++ code/bamazonManager.js
@@ -23,51 +23,24 @@ connection.connect(function (err) {
 function managerView() {
     inquirer
         .prompt([{
-            type: "input",
-            name: "managerUser",
-            message: "Enter manager username:"
+            type: "list",
+            name: "Options",
+            choices: ["View Products For Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
         }]).then(answers => {
-            managerUser = answers.managerUser;
-            if (managerUser === managerUser) {
-                function inputPass() {
-                    inquirer
-                        .prompt([{
-                            type: "password",
-                            name: "mgrPwd",
-                            message: "Enter password (default is admin)"
-                        }]).then(answers => {
-                            password = answers.mgrPwd
-                            if (password === "admin") {
-                                console.log(`Welcome ${managerUser}`)
-                                inquirer
-                                    .prompt([{
-                                        type: "list",
-                                        name: "Options",
-                                        choices: ["View Products For Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
-                                    }]).then(answers => {
-                                        var userChoice = answers.Options
-                                        switch (userChoice) {
-                                            case "View Products For Sale":
-                                                manager.products()
-                                                break;
-                                            case "View Low Inventory":
-                                                manager.lowInv()
-                                                break;
-                                            case "Add to Inventory":
-                                                manager.addInv()
-                                                break;
-                                            case "Add New Product":
-                                                manager.newProd()
-                                                break;
-                                        }
-                                    })
-                            } else {
-                                console.log("That's the wrong password!")
-                            }
-                        })
-                }
-                inputPass()
+            var userChoice = answers.Options
+            switch (userChoice) {
+                case "View Products For Sale":
+                    manager.products()
+                    break;
+                case "View Low Inventory":
+                    manager.lowInv()
+                    break;
+                case "Add to Inventory":
+                    manager.addInv()
+                    break;
+                case "Add New Product":
+                    manager.addProd()
+                    break;
             }
         })
-
 }

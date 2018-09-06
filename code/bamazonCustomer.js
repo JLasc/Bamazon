a0716791332a5e9fcc95661158c2a1b6fbde8beb
JLasc <lascaze.j@gmail.com>
2018-09-06 01:03:12 -0400

    refactored code

--- code/bamazonCustomer.js
+++ code/bamazonCustomer.js
@@ -12,40 +12,36 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-//Connect to DB and run initalizer
-connection.connect(function (err) {
-    if (err) throw err;
-    productInit();
-});
-
 var tableTemp = {
-    head: ['ID', 'Name', 'Dept', 'Price', 'Quantity'],
+    head: ["ID", "Name", "Dept", "Price", "Quantity"],
     colWidths: [6, 13, 13, 13, 13]
-}
+};
 
-
+//Drills down on specific item in table
 function itemTable(id) {
-    connection.query(`SELECT * FROM bamazon.products WHERE item_id=${id}`, function (err, res) {
-
-        var itemTable = new Table(tableTemp)
-        for (i = 0; i < res.length; i++) {
-            tableArr = [
-                res[i].item_id,
-                res[i].product_name,
-                res[i].product_department,
-                res[i].price,
-                res[i].quantity
-            ]
-            itemTable.push(tableArr)
+    connection.query(
+        `SELECT * FROM bamazon.products WHERE item_id=${id}`,
+        function (err, res) {
+            var itemTable = new Table(tableTemp);
+            for (i = 0; i < res.length; i++) {
+                tableArr = [
+                    res[i].item_id,
+                    res[i].product_name,
+                    res[i].product_department,
+                    res[i].price,
+                    res[i].quantity
+                ];
+                itemTable.push(tableArr);
+            }
+            console.log(itemTable.toString());
         }
-        console.log(itemTable.toString());
-    })
-
+    );
 }
 
+//Table that displays all of the products in DB
 function productTable() {
     connection.query("SELECT * FROM bamazon.products", function (err, res) {
-        if (err) throw err
+        if (err) throw err;
         var table = new Table(tableTemp);
         for (i = 0; i < res.length; i++) {
             tableArr = [
@@ -54,53 +50,62 @@ function productTable() {
                 res[i].product_department,
                 res[i].price,
                 res[i].quantity
-            ]
-            table.push(tableArr)
+            ];
+            table.push(tableArr);
         }
         console.log(table.toString());
-    })
+    });
 }
 
+//UI purchase item interaction
 function purchaseQ() {
     inquirer
         .prompt([{
             type: "input",
             name: "quantity",
-            message: "How many would you like to buy?",
-        }]).then(answer => {
-            connection.query(`SELECT * FROM products WHERE item_id=${productId}`, function (err, res) {
-                productQuan = res[0].quantity;
-                var quan = parseInt(answer.quantity);
-                var updQuan = productQuan - quan;
+            message: "How many would you like to buy?"
+        }])
+        .then(answer => {
+            connection.query(
+                `SELECT * FROM products WHERE item_id=${productId}`,
+                function (err, res) {
+                    productQuan = res[0].quantity;
+                    var quan = parseInt(answer.quantity);
+                    var updQuan = productQuan - quan;
 
-                if (productQuan <= 0) {
-                    console.log("Insufficient Quantity!")
-                    inquirer
-                        .prompt([{
-                            type: "confirm",
-                            name: "insuff_confirm",
-                            message: "Would you like to try another item?"
-                        }]).then(answers => {
-                            var userConfirm = answers.insuff_confirm;
-                            if (userConfirm) {
-                                productInit()
-                            } else {
-                                connection.end()
+                    if (productQuan <= 0) {
+                        console.log("Insufficient Quantity!");
+                        inquirer
+                            .prompt([{
+                                type: "confirm",
+                                name: "insuff_confirm",
+                                message: "Would you like to try another item?"
+                            }])
+                            .then(answers => {
+                                var userConfirm = answers.insuff_confirm;
+                                if (userConfirm) {
+                                    productInit();
+                                } else {
+                                    connection.end();
+                                }
+                            });
+                    } else if (productQuan > 0) {
+                        //Update server quantity
+                        connection.query(
+                            `UPDATE products SET quantity=${updQuan} WHERE item_id="${productId}"`,
+                            function (err, res) {
+                                if (err) throw err;
+                                console.log("Purchase Complete!");
+                                setTimeout(productInit, 1);
                             }
-                        })
-                } else if (productQuan > 0) {
-                    //Update server quantity
-                    connection.query(`UPDATE products SET quantity=${updQuan} WHERE item_id="${productId}"`, function (err, res) {
-                        if (err) throw err
-                        console.log("Purchase Complete!")
-                        setTimeout(productInit, 1)
-                    })
+                        );
+                    }
                 }
-            })
-        })
+            );
+        });
 }
 
-
+//User purchase quantity input
 function purchasePrompt() {
     inquirer
         .prompt([{
@@ -111,26 +116,28 @@ function purchasePrompt() {
         .then(answers => {
             productId = parseInt(answers.product_id);
             //Grab and display item with corresponding ID
-            itemTable(productId)
-            // Ask the user how much they want to buy   
-            setTimeout(purchaseQ, 1000)
-        })
+            itemTable(productId);
+            // Ask the user how much they want to buy
+            setTimeout(purchaseQ, 1000);
+        });
 }
 
-
-
-
-//Initialize product menu
+//Initialize product menu on start
 function productInit() {
     //Displays all products
     connection.query("SELECT * FROM bamazon.products;", function (err) {
-        if (err) throw err
+        if (err) throw err;
 
         //Display table with all products
-        productTable()
+        productTable();
 
         //Asks the user to enter ID of product
-        setTimeout(purchasePrompt, 1000)
-
-    })
+        setTimeout(purchasePrompt, 1000);
+    });
 }
+
+//Connect to DB and run initalizer
+connection.connect(function (err) {
+    if (err) throw err;
+    productInit();
+});
